852dba6800df50c0f5e7fea30d19f509b31394aa
Jonathon Menz <jonomenz@gmail.com>
2016-03-12 13:04:10 -0800

    PSR-2 conversion
    The editing convenience of tab-based indentation has been sacrificed for the greater good

--- code/ShareCare.php
+++ code/ShareCare.php
@@ -5,216 +5,246 @@
  * 
  * @extends DataExtension
  */
-class ShareCare extends DataExtension {
-	
-	/**
-	 * Twitter username to be attributed as owner/author of this page.
-	 * Example: 'mytwitterhandle'
-	 * 
-	 * @var string
-	 * @config
-	 */
-	private static $twitter_username = '';
-	
-	/**
-	 * Whether or not to generate a twitter card for this page.
-	 * More info: https://dev.twitter.com/cards/overview
-	 * 
-	 * @var boolean
-	 * @config
-	 */
-	private static $twitter_card = true;
-	
-	/**
-	 * Whether or not to enable a Pinterest preview and fields.
-	 * You need to be using the $PinterestShareLink for this to be useful.
-	 * 
-	 * @var boolean
-	 * @config
-	 */
-	private static $pinterest = false;
-	
-	/**
-	 * Message shown at top of Share tab. Set to false to disable.
-	 * 
-	 * @var string
-	 * @config
-	 */
-	private static $cms_message = 'When this page is shared by people on social media it will look something like this:';
+class ShareCare extends DataExtension
+{
+    /**
+     * Twitter username to be attributed as owner/author of this page.
+     * Example: 'mytwitterhandle'.
+     * 
+     * @var string
+     * @config
+     */
+    private static $twitter_username = '';
 
-	/**
-	 * Add a Social Media tab with a preview of share appearance to the CMS
-	 */
-	public function updateCMSFields(FieldList $fields) {
-		$msg = Config::inst()->get('ShareCare', 'cms_message');
-		if ($msg) {
-			$fields->addFieldToTab("Root.Share", new LiteralField('ShareCareMessage', 
-				'<div class="message notice"><p>' . $msg . '</p></div>'));			
-		}
-		$fields->addFieldToTab("Root.Share", new LiteralField('ShareCarePreview', 
-			$this->owner->RenderWith('ShareCarePreview', array(
-				'IncludeTwitter' => Config::inst()->get('ShareCare', 'twitter_card'),
-				'IncludePinterest' => Config::inst()->get('ShareCare', 'pinterest')
-		))));
-	}
-	
-	/**
-	 * Ensure public URLs are re-scraped by Facebook after publishing
-	 */
-	public function onAfterPublish() {
-		$this->owner->clearFacebookCache();
-	}
-	
-	/**
-	 * Ensure public URLs are re-scraped by Facebook after writing
-	 */
-	public function onAfterWrite() {
-		if (!$this->owner->hasMethod('doPublish')) $this->owner->clearFacebookCache();
-	}
-	
-	/**
-	 * Tell Facebook to re-scrape this URL, if it is accessible to the public
-	 * 
-	 * @return RestfulService_Response
-	 */
-	public function clearFacebookCache() {
-		if ($this->owner->hasMethod('AbsoluteLink')) {
-			$anonymousUser = new Member();
-			if ($this->owner->can("View", $anonymousUser)) {
-				$fetch = new RestfulService('https://graph.facebook.com/');
-				$fetch->setQueryString(array(
-					'id' => $this->owner->AbsoluteLink(),
-					'scrape' => true
-				));
-				return $fetch->request();
-			}
-		}
-	}
+    /**
+     * Whether or not to generate a twitter card for this page.
+     * More info: https://dev.twitter.com/cards/overview.
+     * 
+     * @var bool
+     * @config
+     */
+    private static $twitter_card = true;
 
-	/**
-	 * Generate a URL to share this content on Facebook
-	 * 
-	 * @return string|false
-	 */
-	public function FacebookShareLink() {
-		if (!$this->owner->hasMethod('AbsoluteLink')) return false;
-		$pageURL = urlencode($this->owner->AbsoluteLink());
-		return ($pageURL) ? "https://www.facebook.com/sharer/sharer.php?u=$pageURL" : false;
-	}
+    /**
+     * Whether or not to enable a Pinterest preview and fields.
+     * You need to be using the $PinterestShareLink for this to be useful.
+     * 
+     * @var bool
+     * @config
+     */
+    private static $pinterest = false;
 
-	/**
-	 * Generate a URL to share this content on Twitter
-	 * Specs: https://dev.twitter.com/web/tweet-button/web-intent
-	 * 
-	 * @return string|false
-	 */
-	public function TwitterShareLink() {
-		if (!$this->owner->hasMethod('AbsoluteLink')) return false;
-		$pageURL = urlencode($this->owner->AbsoluteLink());
-		$text = urlencode($this->owner->getOGTitle());
-		return ($pageURL) ? "https://twitter.com/intent/tweet?text=$text&url=$pageURL" : false;
-	}
+    /**
+     * Message shown at top of Share tab. Set to false to disable.
+     * 
+     * @var string
+     * @config
+     */
+    private static $cms_message = 'When this page is shared by people on social media it will look something like this:';
 
-	/**
-	 * Generate a URL to share this content on Google+
-	 * Specs: https://developers.google.com/+/web/snippet/article-rendering
-	 * 
-	 * @return string|false
-	 */
-	public function GooglePlusShareLink() {
-		if (!$this->owner->hasMethod('AbsoluteLink')) return false;
-		$pageURL = urlencode($this->owner->AbsoluteLink());
-		return ($pageURL) ? "https://plus.google.com/share?url=$pageURL" : false;
-	}
+    /**
+     * Add a Social Media tab with a preview of share appearance to the CMS.
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $msg = Config::inst()->get('ShareCare', 'cms_message');
+        if ($msg) {
+            $fields->addFieldToTab('Root.Share', new LiteralField('ShareCareMessage',
+                '<div class="message notice"><p>'.$msg.'</p></div>'));
+        }
+        $fields->addFieldToTab('Root.Share', new LiteralField('ShareCarePreview',
+            $this->owner->RenderWith('ShareCarePreview', array(
+                'IncludeTwitter' => Config::inst()->get('ShareCare', 'twitter_card'),
+                'IncludePinterest' => Config::inst()->get('ShareCare', 'pinterest'),
+        ))));
+    }
 
-	/**
-	 * Generate a URL to share this content on Pinterest
-	 * Specs: https://developers.pinterest.com/pin_it/
-	 * 
-	 * @return string|false
-	 */
-	public function PinterestShareLink() {
-		$pinImage = ($this->owner->hasMethod('getPinterestImage')) ? $this->owner->getPinterestImage() : $this->owner->getOGImage();
-		if ($pinImage) {
-			$imageURL = urlencode($pinImage->getAbsoluteURL());
-			$pageURL = urlencode($this->owner->AbsoluteLink());
-			$description = urlencode($this->owner->getOGTitle());
-			// Combine Title, link and image in to rich link
-			return "http://www.pinterest.com/pin/create/button/?url=$pageURL&media=$imageURL&description=$description";
-		}
-		return false;
-	}
+    /**
+     * Ensure public URLs are re-scraped by Facebook after publishing.
+     */
+    public function onAfterPublish()
+    {
+        $this->owner->clearFacebookCache();
+    }
 
-	/**
-	 * Generate a 'mailto' URL to share this content via Email
-	 * 
-	 * @return string|false
-	 */
-	public function EmailShareLink() {
-		if (!$this->owner->hasMethod('AbsoluteLink')) return false;
-		$pageURL = $this->owner->AbsoluteLink();
-		$subject = urlencode('Thought you might like this');
-		$body = urlencode("Thought of you when I found this: $pageURL");
-		return ($pageURL) ? "mailto:?subject=$subject&body=$body" : false;
-	}
-	
-	/**
-	 * Generate meta tag markup for Twitter Cards
-	 * Specs: https://dev.twitter.com/cards/types/summary-large-image
-	 * 
-	 * @return string
-	 */
-	public function getTwitterMetaTags() {
-		$title = htmlspecialchars($this->owner->getOGTitle());
-		$description = htmlspecialchars($this->owner->getOGDescription());
-		$tMeta = "\n<meta name=\"twitter:title\" content=\"$title\">"
-		."\n<meta name=\"twitter:description\" content=\"$description\">";
-		
-		// If we have a big enough image, include an image tag.
-		$image = $this->owner->getOGImage();
-		// $image may be a string - don't generate a specific twitter tag 
-		// in that case as it is probably the default resource.
-		if ($image instanceof Image && $image->getWidth() >= 280) {
-			$imageURL = htmlspecialchars(Director::absoluteURL($image->Link()));
-			$tMeta.= "\n<meta name=\"twitter:card\" content=\"summary_large_image\">"
-			."\n<meta name=\"twitter:image\" content=\"$imageURL\">";
-		}
-		
-		$username = Config::inst()->get('ShareCare', 'twitter_username');
-		if ($username) {
-			$tMeta.= "\n<meta name=\"twitter:site\" content=\"@$username\">"
-			."\n<meta name=\"twitter:creator\" content=\"@$username\">";
-		}
-		return $tMeta;
-	}
-	
-	/**
-	 * Extension hook for including Twitter Card markup
-	 */
-	public function MetaTags(&$tags) {
-		if (Config::inst()->get('ShareCare', 'twitter_card')) {
-			$tags.= $this->owner->getTwitterMetaTags();
-		}
-	}
-	
-	/**
-	 * The default/fallback value to be used in the 'og:title' open graph tag.
-	 * 
-	 * @return string
-	 */
-	public function getDefaultOGTitle() {
-		return $this->owner->getTitle();
-	}
+    /**
+     * Ensure public URLs are re-scraped by Facebook after writing.
+     */
+    public function onAfterWrite()
+    {
+        if (!$this->owner->hasMethod('doPublish')) {
+            $this->owner->clearFacebookCache();
+        }
+    }
 
-  /**
-	 * The default/fallback Image object or absolute URL to be used in the 'og:image' open graph tag.
-	 * 
-	 * @return Image|string|false
-	 */
-	public function getDefaultOGImage() {
-	  // We don't want to use the SilverStripe logo, so let's use a favicon if available.
-		return (file_exists(BASE_PATH . '/apple-touch-icon.png'))
-			? Director::absoluteURL('apple-touch-icon.png', true)
-			: false;
-  }
+    /**
+     * Tell Facebook to re-scrape this URL, if it is accessible to the public.
+     * 
+     * @return RestfulService_Response
+     */
+    public function clearFacebookCache()
+    {
+        if ($this->owner->hasMethod('AbsoluteLink')) {
+            $anonymousUser = new Member();
+            if ($this->owner->can('View', $anonymousUser)) {
+                $fetch = new RestfulService('https://graph.facebook.com/');
+                $fetch->setQueryString(array(
+                    'id' => $this->owner->AbsoluteLink(),
+                    'scrape' => true,
+                ));
+
+                return $fetch->request();
+            }
+        }
+    }
+
+    /**
+     * Generate a URL to share this content on Facebook.
+     * 
+     * @return string|false
+     */
+    public function FacebookShareLink()
+    {
+        if (!$this->owner->hasMethod('AbsoluteLink')) {
+            return false;
+        }
+        $pageURL = urlencode($this->owner->AbsoluteLink());
+
+        return ($pageURL) ? "https://www.facebook.com/sharer/sharer.php?u=$pageURL" : false;
+    }
+
+    /**
+     * Generate a URL to share this content on Twitter
+     * Specs: https://dev.twitter.com/web/tweet-button/web-intent.
+     * 
+     * @return string|false
+     */
+    public function TwitterShareLink()
+    {
+        if (!$this->owner->hasMethod('AbsoluteLink')) {
+            return false;
+        }
+        $pageURL = urlencode($this->owner->AbsoluteLink());
+        $text = urlencode($this->owner->getOGTitle());
+
+        return ($pageURL) ? "https://twitter.com/intent/tweet?text=$text&url=$pageURL" : false;
+    }
+
+    /**
+     * Generate a URL to share this content on Google+
+     * Specs: https://developers.google.com/+/web/snippet/article-rendering.
+     * 
+     * @return string|false
+     */
+    public function GooglePlusShareLink()
+    {
+        if (!$this->owner->hasMethod('AbsoluteLink')) {
+            return false;
+        }
+        $pageURL = urlencode($this->owner->AbsoluteLink());
+
+        return ($pageURL) ? "https://plus.google.com/share?url=$pageURL" : false;
+    }
+
+    /**
+     * Generate a URL to share this content on Pinterest
+     * Specs: https://developers.pinterest.com/pin_it/.
+     * 
+     * @return string|false
+     */
+    public function PinterestShareLink()
+    {
+        $pinImage = ($this->owner->hasMethod('getPinterestImage')) ? $this->owner->getPinterestImage() : $this->owner->getOGImage();
+        if ($pinImage) {
+            $imageURL = urlencode($pinImage->getAbsoluteURL());
+            $pageURL = urlencode($this->owner->AbsoluteLink());
+            $description = urlencode($this->owner->getOGTitle());
+            // Combine Title, link and image in to rich link
+            return "http://www.pinterest.com/pin/create/button/?url=$pageURL&media=$imageURL&description=$description";
+        }
+
+        return false;
+    }
+
+    /**
+     * Generate a 'mailto' URL to share this content via Email.
+     * 
+     * @return string|false
+     */
+    public function EmailShareLink()
+    {
+        if (!$this->owner->hasMethod('AbsoluteLink')) {
+            return false;
+        }
+        $pageURL = $this->owner->AbsoluteLink();
+        $subject = urlencode('Thought you might like this');
+        $body = urlencode("Thought of you when I found this: $pageURL");
+
+        return ($pageURL) ? "mailto:?subject=$subject&body=$body" : false;
+    }
+
+    /**
+     * Generate meta tag markup for Twitter Cards
+     * Specs: https://dev.twitter.com/cards/types/summary-large-image.
+     * 
+     * @return string
+     */
+    public function getTwitterMetaTags()
+    {
+        $title = htmlspecialchars($this->owner->getOGTitle());
+        $description = htmlspecialchars($this->owner->getOGDescription());
+        $tMeta = "\n<meta name=\"twitter:title\" content=\"$title\">"
+        ."\n<meta name=\"twitter:description\" content=\"$description\">";
+
+        // If we have a big enough image, include an image tag.
+        $image = $this->owner->getOGImage();
+        // $image may be a string - don't generate a specific twitter tag 
+        // in that case as it is probably the default resource.
+        if ($image instanceof Image && $image->getWidth() >= 280) {
+            $imageURL = htmlspecialchars(Director::absoluteURL($image->Link()));
+            $tMeta .= "\n<meta name=\"twitter:card\" content=\"summary_large_image\">"
+            ."\n<meta name=\"twitter:image\" content=\"$imageURL\">";
+        }
+
+        $username = Config::inst()->get('ShareCare', 'twitter_username');
+        if ($username) {
+            $tMeta .= "\n<meta name=\"twitter:site\" content=\"@$username\">"
+            ."\n<meta name=\"twitter:creator\" content=\"@$username\">";
+        }
+
+        return $tMeta;
+    }
+
+    /**
+     * Extension hook for including Twitter Card markup.
+     */
+    public function MetaTags(&$tags)
+    {
+        if (Config::inst()->get('ShareCare', 'twitter_card')) {
+            $tags .= $this->owner->getTwitterMetaTags();
+        }
+    }
+
+    /**
+     * The default/fallback value to be used in the 'og:title' open graph tag.
+     * 
+     * @return string
+     */
+    public function getDefaultOGTitle()
+    {
+        return $this->owner->getTitle();
+    }
+
+    /**
+     * The default/fallback Image object or absolute URL to be used in the 'og:image' open graph tag.
+     * 
+     * @return Image|string|false
+     */
+    public function getDefaultOGImage()
+    {
+        // We don't want to use the SilverStripe logo, so let's use a favicon if available.
+        return (file_exists(BASE_PATH.'/apple-touch-icon.png'))
+            ? Director::absoluteURL('apple-touch-icon.png', true)
+            : false;
+    }
 }
--- code/ShareCareFields.php
+++ code/ShareCareFields.php
@@ -1,127 +1,142 @@
 <?php
 /**
  * Provide default fields and method customisations to complement Open Graph 
- * module with minimal setup
- * 
+ * module with minimal setup.
  */
-class ShareCareFields extends DataExtension {
-	
-	private static $db = array(
-		'OGTitleCustom' => 'Varchar(100)',
-		'OGDescriptionCustom' => 'Varchar(150)'
-	);
+class ShareCareFields extends DataExtension
+{
+    private static $db = array(
+        'OGTitleCustom' => 'Varchar(100)',
+        'OGDescriptionCustom' => 'Varchar(150)',
+    );
 
-	private static $has_one = array(
-		'OGImageCustom' => 'Image',
-		'PinterestImageCustom' => 'Image'
-	);
-	
-	/**
-	 * Message shown above CMS fields. Set to false to disable.
-	 * 
-	 * @var string
-	 * @config
-	 */
-	private static $cms_message = 'The preview above is automatically generated from your content. You can override the default values using these fields:';
+    private static $has_one = array(
+        'OGImageCustom' => 'Image',
+        'PinterestImageCustom' => 'Image',
+    );
 
-	/**
-	 * Add CMS fields to allow setting of custom open graph values
-	 */
-	public function updateCMSFields(FieldList $fields) {
-		$msg = Config::inst()->get('ShareCareFields', 'cms_message');
-		if ($msg) {
-			$fields->addFieldToTab("Root.Share", new LiteralField('ShareCareFieldsMessage', 
-				'<div class="message notice"><p>' . $msg . '</p></div>'));			
-		}
-		$fields->addFieldToTab("Root.Share", TextField::create("OGTitleCustom", 'Share title')
-			->setAttribute('placeholder', $this->owner->getDefaultOGTitle())
-			->setMaxLength(90));
-		$fields->addFieldToTab("Root.Share", TextAreaField::create("OGDescriptionCustom", 'Share description')
-			->setAttribute('placeholder', $this->owner->getDefaultOGDescription())
-			->setRows(2));
-		$fields->addFieldToTab("Root.Share", UploadField::create('OGImageCustom', 'Share image')
-			->setAllowedFileCategories('image')
-			->setAllowedMaxFileNumber(1)
-			->setDescription('<a href="https://developers.facebook.com/docs/sharing/best-practices#images" target="_blank">Optimum image ratio</a> is 1.91:1. (1200px wide by 630px tall or better)'));
-		if (Config::inst()->get('ShareCare', 'pinterest')) {
-			$fields->addFieldToTab("Root.Share", UploadField::create('PinterestImageCustom', 'Pinterest image')
-				->setAllowedFileCategories('image')
-				->setAllowedMaxFileNumber(1)
-				->setDescription('Square/portrait or taller images look best on Pinterest. This image should be at least 750px wide.'));
-		}
-	}
-	
-	/**
-	 * The title that will be used in the 'og:title' open graph tag.
-	 * Use a custom value if set, or fallback to a default value.
-	 * 
-	 * @return string
-	 */
-	public function getOGTitle() {
-		return ($this->owner->OGTitleCustom) ? $this->owner->OGTitleCustom : $this->owner->getDefaultOGTitle();
-	}
+    /**
+     * Message shown above CMS fields. Set to false to disable.
+     * 
+     * @var string
+     * @config
+     */
+    private static $cms_message = 'The preview above is automatically generated from your content. You can override the default values using these fields:';
 
-	/**
-	 * The description that will be used in the 'og:description' open graph tag.
-	 * Use a custom value if set, or fallback to a default value.
-	 * 
-	 * @return string
-	 */
-	public function getOGDescription() {
-		// Use OG Description if set
-		if($this->owner->OGDescriptionCustom) {
-			$description = trim($this->owner->OGDescriptionCustom);
-			if(!empty($description)) return $description;
-		}
-		return $this->owner->getDefaultOGDescription();
-	}
+    /**
+     * Add CMS fields to allow setting of custom open graph values.
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $msg = Config::inst()->get('ShareCareFields', 'cms_message');
+        if ($msg) {
+            $fields->addFieldToTab('Root.Share', new LiteralField('ShareCareFieldsMessage',
+                '<div class="message notice"><p>'.$msg.'</p></div>'));
+        }
+        $fields->addFieldToTab('Root.Share', TextField::create('OGTitleCustom', 'Share title')
+            ->setAttribute('placeholder', $this->owner->getDefaultOGTitle())
+            ->setMaxLength(90));
+        $fields->addFieldToTab('Root.Share', TextAreaField::create('OGDescriptionCustom', 'Share description')
+            ->setAttribute('placeholder', $this->owner->getDefaultOGDescription())
+            ->setRows(2));
+        $fields->addFieldToTab('Root.Share', UploadField::create('OGImageCustom', 'Share image')
+            ->setAllowedFileCategories('image')
+            ->setAllowedMaxFileNumber(1)
+            ->setDescription('<a href="https://developers.facebook.com/docs/sharing/best-practices#images" target="_blank">Optimum image ratio</a> is 1.91:1. (1200px wide by 630px tall or better)'));
+        if (Config::inst()->get('ShareCare', 'pinterest')) {
+            $fields->addFieldToTab('Root.Share', UploadField::create('PinterestImageCustom', 'Pinterest image')
+                ->setAllowedFileCategories('image')
+                ->setAllowedMaxFileNumber(1)
+                ->setDescription('Square/portrait or taller images look best on Pinterest. This image should be at least 750px wide.'));
+        }
+    }
 
-	/**
-	 * The default/fallback value to be used in the 'og:description' open graph tag.
-	 * 
-	 * @return string
-	 */
-	public function getDefaultOGDescription() {		
-		// Use MetaDescription if set
-		if($this->owner->MetaDescription) {
-			$description = trim($this->owner->MetaDescription);
-			if(!empty($description)) return $description;
-		}
-		
-		// Fall back to Content
-		if($this->owner->Content) {
-			$description = trim($this->owner->obj('Content')->Summary(20,5));
-			if(!empty($description)) return $description;
-		}
-		return false;
-	}
+    /**
+     * The title that will be used in the 'og:title' open graph tag.
+     * Use a custom value if set, or fallback to a default value.
+     * 
+     * @return string
+     */
+    public function getOGTitle()
+    {
+        return ($this->owner->OGTitleCustom) ? $this->owner->OGTitleCustom : $this->owner->getDefaultOGTitle();
+    }
+
+    /**
+     * The description that will be used in the 'og:description' open graph tag.
+     * Use a custom value if set, or fallback to a default value.
+     * 
+     * @return string
+     */
+    public function getOGDescription()
+    {
+        // Use OG Description if set
+        if ($this->owner->OGDescriptionCustom) {
+            $description = trim($this->owner->OGDescriptionCustom);
+            if (!empty($description)) {
+                return $description;
+            }
+        }
+
+        return $this->owner->getDefaultOGDescription();
+    }
+
+    /**
+     * The default/fallback value to be used in the 'og:description' open graph tag.
+     * 
+     * @return string
+     */
+    public function getDefaultOGDescription()
+    {
+        // Use MetaDescription if set
+        if ($this->owner->MetaDescription) {
+            $description = trim($this->owner->MetaDescription);
+            if (!empty($description)) {
+                return $description;
+            }
+        }
+
+        // Fall back to Content
+        if ($this->owner->Content) {
+            $description = trim($this->owner->obj('Content')->Summary(20, 5));
+            if (!empty($description)) {
+                return $description;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * The Image object or absolute URL that will be used for the 'og:image' open graph tag.
+     * Use a custom selection if set, or fallback to a default value.
+     * Image size specs: https://developers.facebook.com/docs/sharing/best-practices#images.
+     * 
+     * @return Image|string|false
+     */
+    public function getOGImage()
+    {
+        $ogImage = $this->owner->OGImageCustom();
+        if ($ogImage->exists()) {
+            return ($ogImage->getWidth() > 1200) ? $ogImage->setWidth(1200) : $ogImage;
+        }
+
+        return $this->owner->getDefaultOGImage();
+    }
 
   /**
-	 * The Image object or absolute URL that will be used for the 'og:image' open graph tag.
-	 * Use a custom selection if set, or fallback to a default value.
-	 * Image size specs: https://developers.facebook.com/docs/sharing/best-practices#images
-	 * 
-	 * @return Image|string|false
-	 */
-	public function getOGImage() {
-		$ogImage = $this->owner->OGImageCustom();
-		if ($ogImage->exists()) {
-			return ($ogImage->getWidth() > 1200) ? $ogImage->setWidth(1200) : $ogImage;
-		}
-		return $this->owner->getDefaultOGImage();
-  }
-
-  /**
-	 * Get an Image object to be used in the 'Pin it' ($PinterestShareLink) link.
-   * Image size specs: https://developers.pinterest.com/pin_it/
+   * Get an Image object to be used in the 'Pin it' ($PinterestShareLink) link.
+   * Image size specs: https://developers.pinterest.com/pin_it/.
    * 
    * @return Image|null
    */
-  public function getPinterestImage() {
-		$pinImage = $this->owner->PinterestImageCustom();
-		if ($pinImage->exists()) {
-			return ($pinImage->getWidth() > 1200) ? $pinImage->setWidth(1200) : $pinImage;
-		}
-		return $this->owner->getOGImage();
+  public function getPinterestImage()
+  {
+      $pinImage = $this->owner->PinterestImageCustom();
+      if ($pinImage->exists()) {
+          return ($pinImage->getWidth() > 1200) ? $pinImage->setWidth(1200) : $pinImage;
+      }
+
+      return $this->owner->getOGImage();
   }
 }
